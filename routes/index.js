b19cdbce0c32d152f764da220d3ecbdc6b86b2db
Laetitia <pinto.laetitia@lessonsharing.fr>
2022-01-24 10:19:00 +0100

    Insertion user

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 let router = express.Router();
 const offres = require("../models/offres");
+const users = require("../models/users");
 const { check, validationResult } = require("express-validator");
+const { db } = require('../models/users');
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
@@ -12,13 +14,12 @@ const { check, validationResult } = require("express-validator");
 
 router.post(
     "/connexion", [
-        // Le code doit avoir au moins 2 caractères
-        check("code").isLength({ min: 2 }),
-    ],
-    function(req, res) {
+    // Le code doit avoir au moins 2 caractères
+    check("code").isLength({ min: 2 }),
+],
+    function (req, res) {
         // Test de validité
         const errors = validationResult(req);
-        code = req.body.code;
         if (!errors.isEmpty()) {
             res.render("index", {
                 title: "FaceCast",
@@ -28,27 +29,44 @@ router.post(
             // console.log(code);
         } else {
             // Redirection vers index
-            // console.log("Code :" + code);
-            res.redirect("/");
+            code = req.body.code;
+
+            db.collection('users').find({ 'reservationCode': code }).toArray(function (error, result) {
+                if (error) {
+                    res.send(err);
+                } else {
+                    if (result.length === 0) {
+                        let user = new users({
+                            reservationCode: code
+                        });
+                        user.save(function (err) {
+                            if (err) {
+                                console.log("Insertion non effectuée !");
+                            } else {
+                                console.log("Insertion effectuée !");
+                            }
+                        });
+                    }
+                    res.redirect("/");
+                }
+            });
         }
-    }
-);
-
-
+    });
 
 // Méthode pour la déconnexion
-router.get('/deconnexion', function(req, res, next) {
+router.get('/deconnexion', function (req, res, next) {
     code = "";
     res.redirect("/");
 });
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+    
     offres.find({}, function (err, result) {
         if (err) {
             res.send(err);
         } else {
-            res.render("index", { title: "FaceCast", figuration: result });
+            res.render("index", { title: "FaceCast", figuration: result});
         }
     });
 });
