bf1bb615a541b5012a0c260f4d2ff6c56117ed8d
MeekeHadbi <meeke.hadbi77000@gmail.com>
2022-01-26 18:47:38 +0100

    Redesign

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,10 @@ const { parseString } = require('stylus/lib/utils');
 
 router.post(
     "/connexion", [
-    // Le code doit avoir au moins 2 caractères
-    check("code").isLength({ min: 2 }),
-],
-    function (req, res) {
+        // Le code doit avoir au moins 2 caractères
+        check("code").isLength({ min: 2 }),
+    ],
+    function(req, res) {
         // Test de validité
         const errors = validationResult(req);
         code = req.body.code;
@@ -39,25 +39,25 @@ router.post(
 
 
 // Méthode pour la déconnexion
-router.get('/deconnexion', function (req, res, next) {
+router.get('/deconnexion', function(req, res, next) {
     code = "";
     res.redirect("/");
 });
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    offresFiguration = offres.find({})
+router.get('/', function(req, res, next) {
+    offresFiguration = offres.find({ "vedette": true })
     resaUser = code != null ? reservationOffre.find({ "postulantId": code }) : {}
     Promise.all([
         offresFiguration,
         resaUser
-    ]).then(function ([offresFiguration, resaUser]) {
+    ]).then(function([offresFiguration, resaUser]) {
         res.render('index', { figuration: offresFiguration, reservationsUser: resaUser });
     });
 });
 
 //méthode de réservation d'offre
-router.post("/postuler", function (req, res, next) {
+router.post("/postuler", function(req, res, next) {
     userCode = req.body.code;
     idOffre = req.body.idOffre;
     idEvenement = req.body.idEvenement;
@@ -74,29 +74,26 @@ router.post("/postuler", function (req, res, next) {
         roleDemande: roleDemande,
         description: description,
     })
-    if(typeof usercode != undefined){
-        reservationOffre.find({ "postulantId": userCode, "eventName": eventName, "roleDemande": roleDemande, "description": description }, function (err, results) {
-            if(results.length == 0) {
-                offreSave.save(offreSave, (function (err) {
+    if (typeof usercode != undefined) {
+        reservationOffre.find({ "postulantId": userCode, "eventName": eventName, "roleDemande": roleDemande, "description": description }, function(err, results) {
+            if (results.length == 0) {
+                offreSave.save(offreSave, (function(err) {
                     if (err) {
                         console.log('Erreur de sauvegarde !');
                         res.redirect('/');
-                    }
-                    else {
+                    } else {
                         console.log("save done");
                         res.redirect('/');
                     }
                 }));
-            }
-            else {
+            } else {
                 console.log("doublon évité");
                 res.redirect('/');
             }
         });
-    }
-    else{
+    } else {
         res.redirect('/');
-    } 
+    }
     // TODO décider de ce qu'on fait après la réservation : redirection sur la page des résa user ou vers / ?
 });
 
