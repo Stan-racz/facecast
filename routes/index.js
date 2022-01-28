848f195edddda6ffcb286431275821115d32eeb5
Stan Racz <stan.racz7@gmail.com>
2022-01-28 10:28:44 +0100

    derniers details

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ let router = express.Router();
 const offres = require("../models/offres");
 const { check, validationResult } = require("express-validator");
 const reservationOffre = require('../models/reservationOffre');
+const users = require('../models/users');
 const { parseString } = require('stylus/lib/utils');
+const { useColors } = require('debug/src/browser');
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
@@ -14,10 +16,10 @@ const { parseString } = require('stylus/lib/utils');
 
 router.post(
     "/connexion", [
-        // Le code doit avoir au moins 2 caractères
-        check("code").isLength({ min: 2 }),
-    ],
-    function(req, res) {
+    // Le code doit avoir au moins 2 caractères
+    check("code").isLength({ min: 2 }),
+],
+    function (req, res) {
         // Test de validité
         const errors = validationResult(req);
         code = req.body.code;
@@ -29,8 +31,10 @@ router.post(
             });
             // console.log(code);
         } else {
-            // Redirection vers index
-            // console.log("Code :" + code);
+            users.find({ "reservationCode": code }, function (err, results) {
+                console.log(results)
+            });
+            console.log("Code :" + code);
             res.redirect("/");
         }
     }
@@ -39,25 +43,48 @@ router.post(
 
 
 // Méthode pour la déconnexion
-router.get('/deconnexion', function(req, res, next) {
+router.get('/deconnexion', function (req, res, next) {
     code = "";
     res.redirect("/");
 });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     offresFiguration = offres.find({ "vedette": true })
     resaUser = code != null ? reservationOffre.find({ "postulantId": code }) : {}
+    infoUser = code != null ? users.find({ "reservationCode": code }) : {}
     Promise.all([
         offresFiguration,
-        resaUser
-    ]).then(function([offresFiguration, resaUser]) {
-        res.render('index', { figuration: offresFiguration, reservationsUser: resaUser });
+        resaUser,
+        infoUser
+    ]).then(function ([offresFiguration, resaUser, infoUser]) {
+
+        res.render('index', { figuration: offresFiguration, reservationsUser: resaUser, infoUser: infoUser });
     });
 });
 
+router.get('/user', function (req, res, next) {
+    userCode = req.body.code;
+    nom = req.body.nom;
+    phoneNumber = req.body.phoneNumber;
+    userSave = new User({
+        name:nom,
+        reservationCode:userCode,
+        phoneNumber:phoneNumber,
+    });
+    userSave.save(userSave, (function (err) {
+        if (err) {
+            console.log('Erreur de sauvegarde !');
+            res.redirect('/');
+        } else {
+            console.log("save done");
+            res.redirect('/');
+        }
+    }));
+});
+
 //méthode de réservation d'offre
-router.post("/postuler", function(req, res, next) {
+router.post("/postuler", function (req, res, next) {
     userCode = req.body.code;
     idOffre = req.body.idOffre;
     idEvenement = req.body.idEvenement;
@@ -65,6 +92,12 @@ router.post("/postuler", function(req, res, next) {
     eventType = req.body.eventType;
     roleDemande = req.body.roleDemande;
     description = req.body.description;
+    nbCandidatPostule = req.body.nbCandidatPostule;
+    //je récupere le nb de candidats qui ont postulé, je convertis en nombre, puis je re traduis en string et j'enregistre
+    nbCandidatureCalculation = parseInt(nbCandidatPostule);
+    nbCandidatureCalculation = nbCandidatureCalculation + 1
+    nbCandidatPostule = String(nbCandidatureCalculation);
+
     offreSave = new reservationOffre({
         postulantId: userCode,
         idOffre: idOffre,
@@ -73,11 +106,19 @@ router.post("/postuler", function(req, res, next) {
         eventType: eventType,
         roleDemande: roleDemande,
         description: description,
-    })
+    });
     if (typeof usercode != undefined) {
-        reservationOffre.find({ "postulantId": userCode, "eventName": eventName, "roleDemande": roleDemande, "description": description }, function(err, results) {
+        reservationOffre.find({ "postulantId": userCode, "eventName": eventName, "roleDemande": roleDemande, "description": description }, function (err, results) {
             if (results.length == 0) {
-                offreSave.save(offreSave, (function(err) {
+                offres.updateOne({ "eventName": eventName, "roleDemande": roleDemande, "description": description }, { "nbCandidatPostule": nbCandidatPostule }, function (err, docs) {
+                    if (err) {
+                        console.log(err)
+                    }
+                    else {
+                        console.log("Updated Docs : ", docs);
+                    }
+                });
+                offreSave.save(offreSave, (function (err) {
                     if (err) {
                         console.log('Erreur de sauvegarde !');
                         res.redirect('/');
